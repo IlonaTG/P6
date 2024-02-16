674e4aa91516b3c9ffd332458d6fb2679863aedc
Ilona <ilona.tsivunchyk@gmail.com>
2024-02-16 16:31:05 +0100

    la création du fichier js apart pour tous les changement du site en mode d'authentification

---
+++ FrontEnd/assets/authChanges.js
@@ -0,0 +1,34 @@
+const log = document.querySelector(".log");
+const banner = document.querySelector(".edition-banner");
+const modif = document.querySelector(".mesProjets-changing");
+const container = document.querySelector(".edition-container");
+const filtres = document.querySelector(".filters");
+
+
+function editionActive() {
+  if (localStorage.login) {
+    (log.innerText = "logout"), (banner.style = "display:flex;");
+    filtres.style = "display:none";
+  } else {
+    banner.style = "display:none;";
+    modif.style = "display:none;";
+    container.style = "display:none";
+  }
+}
+editionActive();
+
+log.addEventListener("click", (e) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    e.preventDefault();
+    window.location.reload();
+  }
+  localStorage.removeItem("login");
+  localStorage.removeItem("token");
+  log.innerText = "login";
+  localStorage.clear();
+});
+
+
+
+
