05fe5e48331e0b934b12760c0f698e6ad3072eb3
Ilona <ilona.tsivunchyk@gmail.com>
2024-02-16 16:31:28 +0100

    la création du fichier js apart pour la suppression d'une image

---
+++ FrontEnd/assets/deleteImage.js
@@ -0,0 +1,28 @@
+//Fonction pour supprimer des images
+async function deleteWorks(workId) {
+    try {
+      const token = localStorage.getItem('token');
+      const response = await fetch(`http://localhost:5678/api/works/${workId}`, {
+        method: "DELETE",
+        headers: {
+          accept: "*/*",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+    
+      if (response.ok) {
+        // Supprimer l'élément de la galerie principale
+        const galleryItem = document.querySelector(`[data-work-id="${workId}"]`);
+        if (galleryItem) {
+          galleryItem.remove();
+        }
+    
+        // Mettre à jour la galerie principale
+        await afficherToutesLesImages();
+      } else {
+        console.error("Erreur lors de la suppression de l'image");
+      }
+    } catch (error) {
+      console.error("Erreur lors de la requête de suppression", error);
+    }
+  }
