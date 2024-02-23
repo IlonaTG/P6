268789d99cfac546ec4d9d35f8e7745161c5911d
Ilona <ilona.tsivunchyk@gmail.com>
2024-02-23 23:44:08 +0100

    modifs du style

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -1,20 +1,20 @@
   // Ajout du gestionnaire d'événement pour le formulaire de connexion
-  document.getElementById("login-form").addEventListener("submit", function (e) {
+  document.getElementById('login-form').addEventListener('submit', function (e) {
     e.preventDefault();
     identification();
   });
   
   //Fonction qui se declenche au click du boutton//
   async function identification() {
-    const mail = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
+    const mail = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
     try {
         //On envoie la requete POST à l'API
-      const response = await fetch("http://localhost:5678/api/users/login", {
-        method: "POST",
+      const response = await fetch('http://localhost:5678/api/users/login', {
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json",
-          accept: "application/json",
+          'Content-Type': 'application/json',
+          accept: 'application/json',
         },
         body: JSON.stringify({
           email: mail,
@@ -26,23 +26,22 @@
         //Création du token d'identification//
         const userData = await response.json();
         //Enregistrement du token dans le local storage//
-        localStorage.setItem("token", userData.token);
-        localStorage.setItem("login", true);
+        localStorage.setItem('token', userData.token);
+        localStorage.setItem('login', true);
 
-        window.location.href = "./index.html";
+        window.location.href = './index.html';
         } 
-        
       //Si l'identification est incorrect une alerte s'affiche//
       else {
-        localStorage.setItem("token", undefined);
-        localStorage.setItem("login", undefined);
+        localStorage.setItem('token', undefined);
+        localStorage.setItem('login', undefined);
         alert("La combinaison e-mail/mot de passe est incorrecte.");
       }
     } 
     //Si une erreur se produit on la voit dans la console
     catch (e) {
-      console.error("An error occurred", e);
-      console.error("Error message", e.message);
+      console.error('An error occurred', e);
+      console.error('Error message', e.message);
     }
   }
 
