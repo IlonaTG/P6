326da03091f055e107715e4edcfa80f50f931a9b
Ilona <ilona.tsivunchyk@gmail.com>
2024-02-23 23:43:50 +0100

    suppression d'une duplicata d'appel à l'API

--- FrontEnd/assets/gallery.js
+++ FrontEnd/assets/gallery.js
@@ -1,33 +1,15 @@
-const itemsContainer = document.querySelector(".gallery");
-// Appel à l'API avec fetch
-fetch("http://localhost:5678/api/works/")
-  .then((response) => response.json())
-  .then((data) => {
-    data.forEach((work) => {
-      const figure = document.createElement("figure");
-      const image = document.createElement("img");
-      const caption = document.createElement("figcaption");
-      image.src = work.imageUrl;
-      caption.textContent = work.title;
-      //Ajouter l'image a la gallerie
-      itemsContainer.appendChild(figure);
-      figure.appendChild(image);
-      figure.appendChild(caption);
-    });
-  });
-  
-
+const itemsContainer = document.querySelector('.gallery');
 // Fonction pour ajouter les travaux à la galerie
 async function afficherToutesLesImages() {
-  itemsContainer.innerHTML = ""; // Effacer les éléments actuels dans le conteneur
+  itemsContainer.innerHTML = '';
   // Une requête Fetch pour obtenir les données de l'API
-  const response = await fetch("http://localhost:5678/api/works/")
+  const response = await fetch('http://localhost:5678/api/works/')
   .then((response) => response.json())
   .then((data) => {
     data.forEach(element => {
-      const figure = document.createElement("figure");
-      const image = document.createElement("img");
-      const caption = document.createElement("figcaption");
+      const figure = document.createElement('figure');
+      const image = document.createElement('img');
+      const caption = document.createElement('figcaption');
       image.src = element.imageUrl;
       caption.textContent = element.title;
 
@@ -38,13 +20,13 @@ async function afficherToutesLesImages() {
   });
 };
 
-
+afficherToutesLesImages();
 
 // Fonction de tri par catégorie
 async function trierParCategorie(categorie) {
-    itemsContainer.innerHTML = "";
+    itemsContainer.innerHTML = '';
     // Une requête Fetch pour obtenir les données de l'API
-    const response = await fetch("http://localhost:5678/api/works/")
+    const response = await fetch('http://localhost:5678/api/works/')
       .then((response) => response.json())
       .then((data) => {
         // Filtrer les éléments en fonction de la catégorie sélectionnée
@@ -53,9 +35,9 @@ async function trierParCategorie(categorie) {
         );
         // Parcourir les éléments filtrés et les ajouter au conteneur
         elementsFiltres.forEach((element) => {
-          const figure = document.createElement("figure");
-          const image = document.createElement("img");
-          const caption = document.createElement("figcaption");
+          const figure = document.createElement('figure');
+          const image = document.createElement('img');
+          const caption = document.createElement('figcaption');
           image.src = element.imageUrl;
           caption.textContent = element.title;
   
@@ -68,22 +50,22 @@ async function trierParCategorie(categorie) {
   trierParCategorie();
   
   // Ajouter des écouteurs d'événements pour les boutons de tri
-  const tousBtn = document.createElement("button");
-  tousBtn.textContent = "Tous";
-  tousBtn.addEventListener("click", afficherToutesLesImages);
-  document.querySelector(".filters").appendChild(tousBtn);
+  const tousBtn = document.createElement('button');
+  tousBtn.textContent = 'Tous';
+  tousBtn.addEventListener('click', afficherToutesLesImages);
+  document.querySelector('.filters').appendChild(tousBtn);
   
-  const objetsBtn = document.createElement("button");
-  objetsBtn.textContent = "Objets";
-  objetsBtn.addEventListener("click", () => trierParCategorie("Objets"));
-  document.querySelector(".filters").appendChild(objetsBtn);
+  const objetsBtn = document.createElement('button');
+  objetsBtn.textContent = 'Objets';
+  objetsBtn.addEventListener('click', () => trierParCategorie('Objets'));
+  document.querySelector('.filters').appendChild(objetsBtn);
   
-  const appartementsBtn = document.createElement("button");
-  appartementsBtn.textContent = "Appartements";
-  appartementsBtn.addEventListener("click", () => trierParCategorie("Appartements"));
-  document.querySelector(".filters").appendChild(appartementsBtn);
+  const appartementsBtn = document.createElement('button');
+  appartementsBtn.textContent = 'Appartements';
+  appartementsBtn.addEventListener('click', () => trierParCategorie('Appartements'));
+  document.querySelector('.filters').appendChild(appartementsBtn);
   
-  const hotelsBtn = document.createElement("button");
-  hotelsBtn.textContent = "Hotels & restaurants";
-  hotelsBtn.addEventListener("click", () => trierParCategorie("Hotels & restaurants"));
-  document.querySelector(".filters").appendChild(hotelsBtn);
+  const hotelsBtn = document.createElement('button');
+  hotelsBtn.textContent = 'Hotels & restaurants';
+  hotelsBtn.addEventListener('click', () => trierParCategorie('Hotels & restaurants'));
+  document.querySelector('.filters').appendChild(hotelsBtn);
