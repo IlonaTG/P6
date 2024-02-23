5118a572e6f812cafa9a692b90d20baf4520d950
Ilona <ilona.tsivunchyk@gmail.com>
2024-02-23 23:41:23 +0100

    modifs du style

--- FrontEnd/assets/authChanges.js
+++ FrontEnd/assets/authChanges.js
@@ -1,31 +1,31 @@
-const log = document.querySelector(".log");
-const banner = document.querySelector(".edition-banner");
-const modif = document.querySelector(".mesProjets-changing");
-const container = document.querySelector(".edition-container");
-const filtres = document.querySelector(".filters");
+const log = document.querySelector('.log');
+const banner = document.querySelector('.edition-banner');
+const modif = document.querySelector('.mesProjets-changing');
+const container = document.querySelector('.edition-container');
+const filtres = document.querySelector('.filters');
 
 
 function editionActive() {
   if (localStorage.login) {
-    (log.innerText = "logout"), (banner.style = "display:flex;");
-    filtres.style = "display:none";
+    (log.innerText = 'logout'), (banner.style = 'display:flex;');
+    filtres.style = 'display:none';
   } else {
-    banner.style = "display:none;";
-    modif.style = "display:none;";
-    container.style = "display:none";
+    banner.style = 'display:none;';
+    modif.style = 'display:none;';
+    container.style = 'display:none';
   }
 }
 editionActive();
 
-log.addEventListener("click", (e) => {
-  const token = localStorage.getItem("token");
+log.addEventListener('click', (e) => {
+  const token = localStorage.getItem('token');
   if (token) {
     e.preventDefault();
     window.location.reload();
   }
-  localStorage.removeItem("login");
-  localStorage.removeItem("token");
-  log.innerText = "login";
+  localStorage.removeItem('login');
+  localStorage.removeItem('token');
+  log.innerText = 'login';
   localStorage.clear();
 });
 
